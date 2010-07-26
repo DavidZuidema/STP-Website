e157810524bb8b40cec3b65068ac49f9e44395b6
David Craig Zuidema <dczuidema@gmail.com>
2010-07-26 14:17:05 -0400

    reformatted response text for contact.php

--- contact.php
+++ contact.php
@@ -47,7 +47,7 @@
 
 					   mail($to, $subject, $body);
 
-					  echo "<h3>Thanks for the message!</h3><h4>David responds to his email twice daily and always within 24 hours.</h4>"; 
+					  echo "<h3>Thanks for the message!</h3><h5>I respond to my email twice daily and always within 24 hours.</h5><h5>-David</h5>"; 
 					?>
 					<h4><a href="index.htm">Home</a></h4>
 				</div>
